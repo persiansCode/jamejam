99b69e52fe1fc1220b830467aafbb6e82d9cd04f
mohammadreza <mohamadreza.mirzaii875@gmail.com>
2026-05-24 20:50:06 +0330

    dfdf

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
       // یا
     Paginator::defaultView('pagination::tailwind');
     Paginator::defaultSimpleView('pagination::simple-tailwind');
-        
+        if (app()->environment('production') || config('app.env') === 'production') {
+        URL::forceScheme('https');
+    }
     }
 }
